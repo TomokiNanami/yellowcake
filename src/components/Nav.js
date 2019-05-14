cb9f349a12f82b7578424e52e6a6499d88c1c092
masak <oicwam.orz1228@gmail.com>
2019-05-14 21:31:17 +0900

    careerのページを作成

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -50,6 +50,7 @@ export class Navigation extends Component {
           </Link>
           <div className="Nav--Links">
             <NavLink to="/">Home</NavLink>
+            <NavLink to="/career-about">Career</NavLink>
             <div
               className={`Nav--Group ${
                 this.state.activeSubNav === 'posts' ? 'active' : ''
